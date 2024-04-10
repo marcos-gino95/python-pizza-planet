793465d17b7d07b14a4ece4aeb0b971b1422234c
Marcos <mark.metalgino@gmail.com>
2024-04-09 23:55:09 -0500

    feat: add BeverageManager

--- app/repositories/models.py
+++ app/repositories/models.py
@@ -46,6 +46,7 @@ class Beverage(db.Model):
 
 class OrderBeverage(db.Model):
     _id = db.Column(db.Integer, primary_key=True)
+    beverage_price = db.Column(db.Float)
     order_id = db.Column(db.Integer, db.ForeignKey("order._id"))
     beverage_id = db.Column(db.Integer, db.ForeignKey("beverage._id"))
     beverage = db.relationship("Beverage", backref=db.backref("beverage"))
