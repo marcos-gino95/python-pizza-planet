8e0da520dbdd41b03bf7c8233e5c1996fa376d31
Marcos Hernadez <37634946+marcos-gino95@users.noreply.github.com>
2024-04-12 20:11:35 -0500

    Piz2 add a beverage model manager and service (#3)
    * feat: add Beverage and orderBeverage models
    
    * feat: add BeverageSerializer and OrderBeverageSerializer
    
    * feat: add BeverageManager
    
    * feat: add beverage controller
    
    * feat: add beverage services
    
    * feat: add beverage price in order
    
    * test: add tests for beverage in controllers
    
    * test: add beverage test services
    
    * test: fix test_order with beverage

--- app/repositories/serializers.py
+++ app/repositories/serializers.py
@@ -1,5 +1,5 @@
 from app.plugins import ma
-from .models import Ingredient, Size, Order, OrderDetail
+from .models import Ingredient, OrderBeverage, Size, Order, OrderDetail, Beverage
 
 
 class IngredientSerializer(ma.SQLAlchemyAutoSchema):
@@ -7,7 +7,7 @@ class IngredientSerializer(ma.SQLAlchemyAutoSchema):
     class Meta:
         model = Ingredient
         load_instance = True
-        fields = ('_id', 'name', 'price')
+        fields = ("_id", "name", "price")
 
 
 class SizeSerializer(ma.SQLAlchemyAutoSchema):
@@ -15,7 +15,15 @@ class SizeSerializer(ma.SQLAlchemyAutoSchema):
     class Meta:
         model = Size
         load_instance = True
-        fields = ('_id', 'name', 'price')
+        fields = ("_id", "name", "price")
+
+
+class BeverageSerializer(ma.SQLAlchemyAutoSchema):
+
+    class Meta:
+        model = Beverage
+        load_instance = True
+        fields = ("_id", "name", "price")
 
 
 class OrderDetailSerializer(ma.SQLAlchemyAutoSchema):
@@ -25,27 +33,39 @@ class OrderDetailSerializer(ma.SQLAlchemyAutoSchema):
     class Meta:
         model = OrderDetail
         load_instance = True
+        fields = ("ingredient_price", "ingredient")
+
+
+class OrderBeverageSerializer(ma.SQLAlchemyAutoSchema):
+
+    beverage = ma.Nested(BeverageSerializer)
+
+    class Meta:
+        model = OrderBeverage
+        load_instance = True
         fields = (
-            'ingredient_price',
-            'ingredient'
+            "beverage_price",
+            "beverage",
         )
 
 
 class OrderSerializer(ma.SQLAlchemyAutoSchema):
     size = ma.Nested(SizeSerializer)
     detail = ma.Nested(OrderDetailSerializer, many=True)
+    beverage = ma.Nested(OrderBeverageSerializer, many=True)
 
     class Meta:
         model = Order
         load_instance = True
         fields = (
-            '_id',
-            'client_name',
-            'client_dni',
-            'client_address',
-            'client_phone',
-            'date',
-            'total_price',
-            'size',
-            'detail'
+            "_id",
+            "client_name",
+            "client_dni",
+            "client_address",
+            "client_phone",
+            "date",
+            "total_price",
+            "size",
+            "detail",
+            "beverage",
         )
