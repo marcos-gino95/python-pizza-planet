adfc4d503885e83a63e51e2e9e1613f9d1d08e05
Marcos <mark.metalgino@gmail.com>
2024-04-14 10:16:32 -0500

    feat: add fake data file

---
+++ seeds/data.py
@@ -0,0 +1,36 @@
+sizes = {"Personal": 3, "Small": 10, "Medium": 15, "Large": 20, "Familiar": 25}
+
+ingredients = {
+    "cheese": 2,
+    "pepperoni": 3,
+    "mushrooms": 2,
+    "onions": 1,
+    "bell peppers": 3,
+    "olives": 2,
+    "tomatoes": 2,
+    "sausage": 4,
+    "bacon": 4,
+    "pineapple": 2,
+}
+
+beverages = {
+    "Coca-Cola": 5,
+    "Fanta": 2,
+    "Gallito": 3,
+    "Frutis": 2,
+    "Sprite": 5,
+    "Water": 1,
+}
+
+clients = [
+    "John Doe",
+    "Alice Smith",
+    "Bob Johnson",
+    "Emma Williams",
+    "Michael Brown",
+    "Sophia Jones",
+    "William Garcia",
+    "Olivia Martinez",
+    "James Davis",
+    "Charlotte Rodriguez",
+]
