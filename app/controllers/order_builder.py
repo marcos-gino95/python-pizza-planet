af956ed792f2fac0335dcea903f8ccb66dcefd65
Marcos <mark.metalgino@gmail.com>
2024-04-14 23:58:39 -0500

    feat: create order builder

---
+++ app/controllers/order_builder.py
@@ -0,0 +1,29 @@
+from __future__ import annotations
+from abc import ABC, abstractmethod
+
+
+class OrderBuilder(ABC):
+    """
+    The OrderBuilder interface specifies methods for creating the different parts of
+    the Order objects.
+    """
+
+    @abstractmethod
+    def set_data(self, data) -> None:
+        pass
+
+    @abstractmethod
+    def check_required_info(self) -> None:
+        pass
+
+    @abstractmethod
+    def check_size(self) -> None:
+        pass
+
+    @abstractmethod
+    def calculate_order_price(self) -> None:
+        pass
+
+    @abstractmethod
+    def create(self) -> None:
+        pass
